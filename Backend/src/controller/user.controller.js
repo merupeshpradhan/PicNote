fbf5c73cedb65b100005054544f9c465f4d7e890
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-09-11 18:11:44 +0530

    Implement user authentication: add signup, login, and logout functionality; integrate password hashing and JWT token generation

--- Backend/src/controller/user.controller.js
+++ Backend/src/controller/user.controller.js
@@ -1,9 +1,94 @@
+import { User } from "../models/user.schema.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
-const userSignup = asyncHandler(async (req, res) => {});
-const userLogin = asyncHandler(async (req, res) => {});
-const userLogout = asyncHandler(async (req, res) => {});
+const userSignup = asyncHandler(async (req, res) => {
+  const { userName, email, password } = req.body;
+
+  const avatarLocalPath = req.file?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400,"User cannot upload here image.");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  if (!userName || !email || !password) {
+    throw new ApiError(400,"New user please provide your all detials.");
+  }
+
+  const otherEmail = await User.findOne({ email });
+  if (otherEmail) {
+    throw new ApiError(400,"This email user already exist in our area.");
+  }
+
+  const user = await User.create({
+    avatar,
+    userName,
+    email,
+    password,
+  });
+
+  const userData = {
+    id: user._id,
+    avatar: user.avatar,
+    userName: user.userName,
+    email: user.email,
+  };
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, userData, "User register successfully."));
+});
+
+const userLogin = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new ApiError(400,"Please provide email and password.");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new ApiError(400,"Please provide correct email.");
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "Incorrect password.");
+  }
+
+  const accessToken = await user.generateAccessToken();
+  const refreshToken = await user.generateRefreshToken();
+
+  const option = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "Strict",
+  };
+
+  res.cookie("accessToken", accessToken, option);
+  res.cookie("refreshToken", refreshToken, option);
+
+  const userData = {
+    id: user._id,
+    avatar: user.avatar,
+    userName: user.userName,
+    email: user.email,
+  };
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, userData, "User login successfully."));
+});
+const userLogout = asyncHandler(async (req, res) => {
+  res.clearCookie("accessToken");
+  res.clearCookie("refreshToken");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "User logout successfuly."));
+});
 
 export { userSignup, userLogin, userLogout };
--- Backend/src/models/user.schema.js
+++ Backend/src/models/user.schema.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -17,5 +19,48 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+// Hashing password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Compare password
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// Access and Refresh token
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      userName: this.userName,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
