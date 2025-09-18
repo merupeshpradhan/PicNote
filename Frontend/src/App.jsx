3bc33a4d2545f1680f59b431ce0b3634c2ffc916
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-09-18 22:06:27 +0530

    Add main application structure with routing and basic components: Navbar, Home, Login, Register, and Post

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./assets/components/Navbar";
+import Home from "./assets/pages/Home";
+import Login from "./assets/pages/Login";
+import Register from "./assets/pages/Register";
+import Post from "./assets/pages/post";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/post" element={<Post />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
