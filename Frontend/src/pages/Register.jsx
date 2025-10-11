d60b133a4157b6c5de01e68a66bb3a53189c584a
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-10-11 12:39:22 +0530

    Add Sign-up illustration and enhance Register component layout for improved user experience

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import { FaEye, FaEyeSlash, FaRegImage } from "react-icons/fa";
+import { FaEye, FaEyeSlash, FaRegImage, FaUser } from "react-icons/fa";
+import { HiUserGroup } from "react-icons/hi2";
 import { toast } from "react-toastify";
 
 function Register() {
@@ -16,12 +17,10 @@ function Register() {
 
   const userRegister = async (e) => {
     e.preventDefault();
-
     setLoading(true);
 
     try {
       const formData = new FormData();
-
       formData.append("avatar", avatar);
       formData.append("userName", userName);
       formData.append("email", email);
@@ -34,9 +33,7 @@ function Register() {
       );
 
       console.log("You register successfully!", res);
-
-      toast.success("You register successfully");
-
+      toast.success("You registered successfully!");
       navigate("/login");
 
       setAvatar("");
@@ -44,17 +41,10 @@ function Register() {
       setEmail("");
       setPassword("");
     } catch (error) {
-      // console.log(
-      //   "Please send your all detials.",
-      //   error.response?.data?.message || error.message
-      // );
-
       if (error.response) {
         toast.error(error.response.data.message);
       } else {
-        toast.error(
-          "Oops! Something went wrong. Please check and complete all your details."
-        );
+        toast.error("Oops! Something went wrong. Please try again.");
       }
     } finally {
       setLoading(false);
@@ -62,117 +52,146 @@ function Register() {
   };
 
   return (
-    <div className="h-[100vh] w-full flex justify-center items-center">
-      <div className="register-container border-2 border-sky-200 shadow-lg shadow-sky-300 rounded-2xl h-[full] w-[25%] flex flex-col">
-        <div className="flex justify-center py-2">
-          <h1 className="text-xl font-bold tracking-wider text-red-500">
-            Register
-          </h1>
+    <div className="min-h-screen flex items-center justify-center bg-pink-100 px-4">
+      <div className="bg-white rounded-2xl shadow-2xl flex flex-col md:flex-row max-w-5xl w-full overflow-hidden">
+        {/* Left Section */}
+        <div className="md:w-1/2 bg-pink-50 flex flex-col items-center p-6">
+          {/* Project Name */}
+          <div className="text-center mb-6 mt-[17px]">
+            <h1 className="text-4xl font-extrabold text-pink-600 tracking-wide">
+              Pic<span className="text-gray-800">Note</span>
+            </h1>
+            <p className="text-gray-500 text-sm mt-1">
+              Capture. Save. Remember.
+            </p>
+          </div>
+
+          {/* Illustration  */}
+          <div className="relative">
+            <img src="/Sign-up-amico.png" alt="Preview" className="" />
+          </div>
         </div>
-        <form
-          onSubmit={userRegister}
-          className="register-form flex flex-col gap-5 p-5"
-        >
-          <div className="avatar-section flex gap-2 justify-around items-center">
-            <div className="avatar-preview-container border-2 border-sky-500 xl:h-[20vh] xl:w-[42%] rounded-md flex justify-center items-center overflow-hidden">
-              {previewAvatar ? (
-                <div className="avatar-preview">
-                  <img
-                    src={previewAvatar}
-                    alt="Avatar preview"
-                    className="w-full h-full object-cover "
-                  />
-                </div>
-              ) : (
-                <div className="font-semibold text-sky-500">
-                  <FaRegImage size={40} />
-                </div>
-              )}
-            </div>
-            <div className="avatar-selection">
-              <button
-                type="button"
-                onClick={() => document.getElementById("avtarInput").click()}
-                className="bg-sky-500 px-4 py-2 rounded-full font-semibold text-white text-[14px] hover:bg-sky-800 cursor-pointer transition duration-200 tracking-wider"
-              >
-                {avatar ? "Change your image" : "Provide your image"}
-              </button>
+
+        {/* Right Section - Form */}
+        <div className="md:w-1/2 p-8 md:p-12">
+           {/* Form Header */}
+          <h2 className="text-2xl md:text-3xl font-semibold text-gray-700 mb-2">
+            Create Your Account
+          </h2>
+          <p className="text-gray-500 mb-6">
+            Join PicNote to save, organize, and remember your notes and images securely.
+          </p>
+
+          <form onSubmit={userRegister} className="space-y-5">
+            {/* Avatar Upload */}
+            <div className="flex gap-7">
+              <div className="avatar-preview-container border-2 border-pink-500 xl:h-[20vh] xl:w-[42%] rounded-md flex justify-center items-center overflow-hidden">
+                {previewAvatar ? (
+                  <div className="avatar-preview">
+                    <img
+                      src={previewAvatar}
+                      alt="Avatar preview"
+                      className="w-full h-full object-cover "
+                    />
+                  </div>
+                ) : (
+                  <div className="font-semibold text-pink-500">
+                    <FaUser size={40} />
+                  </div>
+                )}
+              </div>
+              <div className="flex items-center gap-3">
+                <button
+                  type="button"
+                  onClick={() => document.getElementById("avatarInput").click()}
+                  className="bg-pink-500 text-white py-2 px-4 rounded-md font-semibold hover:bg-pink-600 transition cursor-pointer"
+                >
+                  {avatar ? "Change your image" : "Provide your image"}
+                </button>
+                <FaRegImage className="text-pink-500 text-xl" />
+              </div>
               <input
-                className="hidden"
-                id="avtarInput"
+                id="avatarInput"
                 type="file"
                 accept="image/*"
-                placeholder="Provide your own image"
+                className="hidden"
                 onChange={(e) => {
                   const file = e.target.files[0];
                   setAvatar(file);
                   if (file) {
                     setAvatarPreview(URL.createObjectURL(file));
                   } else {
-                    setAvatarPreview(null);
+                    setAvatarPreview("");
                   }
                 }}
               />
             </div>
-          </div>
-          <div className="fullName flex gap-2 justify-between items-center">
-            <label className="font-semibold text-gray-700">Full Name</label>
-            <input
-              type="text"
-              placeholder="Enter your full name"
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-              className="border-2 border-sky-400 focus:border-sky-600 rounded-md xl:w-[75%] xl:py-1.5 p-1.5 outline-0 font-mono font-medium tracking-wide xl:text-[16px]"
-            />
-          </div>
-          <div className="email flex gap-2 justify-between items-center">
-            <label className="font-semibold text-gray-700">Email</label>
-            <input
-              type="text"
-              placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="border-2 border-sky-400 focus:border-sky-600 rounded-md xl:w-[75%] xl:py-1.5 p-1.5 outline-0 font-mono font-medium tracking-wide xl:text-[16px]"
-            />
-          </div>
-          <div className="password flex gap-2 justify-between items-center relative">
-            <label className="font-semibold text-gray-700">Password</label>
-            <div className="xl:w-[75%] relative">
-              <input
-                type={showPassword ? "text" : "password"}
-                placeholder="Enter your password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="border-2 border-sky-400 focus:border-sky-600 rounded-md xl:w-full xl:py-1.5 p-1.5 outline-0 font-mono font-medium tracking-wide xl:text-[16px]"
-              />
-              <button
-                type="button"
-                onClick={() => setShowPassword(!showPassword)}
-                className="absolute inset-y-0 right-2 flex items-center text-gray-600 hover:text-gray-900  cursor-pointer"
-              >
-                {showPassword ? <FaEyeSlash /> : <FaEye />}
-              </button>
-            </div>
-          </div>
 
-          <div className="submit-button flex justify-center">
+            {/* Full Name */}
+            <div>
+              <label className="font-semibold text-gray-700">Full Name</label>
+              <input
+                type="text"
+                placeholder="Enter your full name"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+                className="w-full border border-pink-300 rounded-md p-2 focus:border-pink-500 focus:outline-none"
+              />
+            </div>
+
+            {/* Email */}
+            <div>
+              <label className="font-semibold text-gray-700">E-mail</label>
+              <input
+                type="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full border border-pink-300 rounded-md p-2 focus:border-pink-500 focus:outline-none"
+              />
+            </div>
+
+            {/* Password */}
+            <div className="relative">
+              <label className="font-semibold text-gray-700">Password</label>
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Enter your password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="w-full border border-pink-300 rounded-md p-2 pr-10 focus:border-pink-500 focus:outline-none"
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute inset-y-0 right-2 flex items-center text-gray-500 hover:text-gray-700"
+                >
+                  {showPassword ? <FaEyeSlash /> : <FaEye />}
+                </button>
+              </div>
+            </div>
+
+            {/* Submit Button */}
             <button
               type="submit"
-              className="bg-red-500 xl:py-1.5 xl:w-[33%] rounded-lg text-white xl:font-semibold xl:tracking-wider text-[18px] xl:mt-2 hover:bg-red-700 cursor-pointer transition duration-200"
+              disabled={loading}
+              className="w-full bg-pink-500 text-white py-2 rounded-md font-semibold hover:bg-pink-600 transition cursor-pointer"
             >
-              {loading ? "Processing..." : "Register"}
+              {loading ? "Processing..." : "Sign Up"}
             </button>
-          </div>
-        </form>
-        <div className="link-to-login py-1">
-          <div className=" flex justify-center">
-            <hr className="w-[95%] border-sky-500" />
-          </div>
-          <Link to={"/login"} className="flex justify-center p-5 ">
-            <button className="bg-green-500 xl:px-7 xl:py-1.5 xl:w-[90%]  rounded-lg xl:font-semibold xl:tracking-wider text-[18px] text-white hover:bg-green-700 cursor-pointer transition duration-200">
+          </form>
+
+          {/* Login Redirect */}
+          <p className="text-center text-gray-600 mt-6">
+            Already have an account?{" "}
+            <Link
+              to="/login"
+              className="text-pink-500 font-semibold hover:underline"
+            >
               Login
-            </button>
-          </Link>
+            </Link>
+          </p>
         </div>
       </div>
     </div>
