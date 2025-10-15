561fcb7954d257d6a0d5c9266b81f8736f3f5001
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-10-15 21:08:00 +0530

    Refactor Login and Register components for improved layout and responsiveness

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -44,30 +44,30 @@ function Login() {
     <div className="min-h-screen flex items-center justify-center bg-pink-100 px-4">
       <div className="bg-white rounded-2xl shadow-2xl flex flex-col md:flex-row max-w-5xl w-full overflow-hidden justify-center items-center">
         {/* Left Section - Logo + Illustration */}
-        <div className="md:w-1/2 bg-pink-50 flex flex-col items-center justify-center p-6">
-          <div className="text-center mb-6  mt-[17px]">
-            <h1 className="text-4xl font-extrabold text-pink-600 tracking-wide">
+        <div className="w-full md:w-1/2 bg-pink-50 flex md:flex-col items-center justify-between p-3 md:p-6">
+          <div className="text-center mb-6 mt-[17px]">
+            <h1 className="text-2xl md:text-4xl font-extrabold text-pink-600 tracking-wide">
               Pic<span className="text-gray-800">Note</span>
             </h1>
-            <p className="text-gray-500 text-sm mt-1">
+            <p className="text-gray-500 text-sm md:mt-1">
               Capture. Save. Remember.
             </p>
           </div>
            <div className="relative flex items-center justify-center">
-            <img src="/Tablet-login-bro.png" alt="Preview" className="" />
+            <img src="/Tablet-login-bro.png" alt="Preview" className="w-[150px] md:w-full" />
           </div>
         </div>
 
         {/* Right Section - Login Form */}
-        <div className="md:w-1/2 p-8 md:p-12">
-          <h2 className="text-2xl md:text-3xl font-semibold text-gray-700 mb-2">
+        <div className="md:w-1/2 pt-4 pb-4 px-6 md:p-12">
+          <h2 className="text-xl md:text-3xl font-semibold text-gray-700 mb-1 md:mb-2">
             Login to Your Account
           </h2>
-          <p className="text-gray-500 mb-6">
+          <p className="text-gray-500 text-sm sm:text-md lg:text-[15px] mb-3 md:mb-6">
             Welcome back! Please enter your details to continue to PicNote.
           </p>
 
-          <form onSubmit={userLogin} className="space-y-5">
+          <form onSubmit={userLogin} className="space-y-1 md:space-y-5">
             {/* Email */}
             <div>
               <label className="font-semibold text-gray-700">Email</label>
@@ -76,7 +76,7 @@ function Login() {
                 placeholder="Enter your email"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
-                className="w-full border border-pink-300 rounded-md p-2 focus:border-pink-500 focus:outline-none"
+                className="w-full border border-pink-300 rounded-md p-1.5 md: focus:border-pink-500 focus:outline-none"
               />
             </div>
 
@@ -89,7 +89,7 @@ function Login() {
                   placeholder="Enter your password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  className="w-full border border-pink-300 rounded-md p-2 pr-10 focus:border-pink-500 focus:outline-none"
+                  className="w-full border border-pink-300 rounded-md p-1.5 md:p-2 focus:border-pink-500 focus:outline-none"
                 />
                 <button
                   type="button"
@@ -105,14 +105,14 @@ function Login() {
             <button
               type="submit"
               disabled={loading}
-              className="w-full bg-pink-500 text-white py-2 rounded-md font-semibold hover:bg-pink-600 transition"
+              className="w-full bg-pink-500 mt-3 md:mt-0 text-white py-2 rounded-md font-semibold hover:bg-pink-600 transition"
             >
               {loading ? "Processing..." : "Sign In"}
             </button>
           </form>
 
           {/* Register Redirect */}
-          <p className="text-center text-gray-600 mt-6">
+          <p className="text-center text-gray-600 mt-3 md:mt-6">
             Don't have an account?
             <Link
               to="/register"
--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -52,36 +52,41 @@ function Register() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-pink-100 px-4">
-      <div className="bg-white rounded-2xl shadow-2xl flex flex-col md:flex-row max-w-5xl w-full overflow-hidden">
+      <div className="md:bg-white md:rounded-2xl md:shadow-2xl flex flex-col md:flex-row max-w-5xl w-full overflow-hidden">
         {/* Left Section */}
-        <div className="md:w-1/2 bg-pink-50 flex flex-col items-center p-6">
+        <div className="w-full md:w-1/2 med:bg-pink-50 flex md:flex-col items-center justify-between p-3 md:p-6">
           {/* Project Name */}
           <div className="text-center mb-6 mt-[17px]">
-            <h1 className="text-4xl font-extrabold text-pink-600 tracking-wide">
+            <h1 className="text-2xl md:text-4xl font-extrabold text-pink-600 tracking-wide">
               Pic<span className="text-gray-800">Note</span>
             </h1>
-            <p className="text-gray-500 text-sm mt-1">
+            <p className="text-gray-500 text-sm md:mt-1">
               Capture. Save. Remember.
             </p>
           </div>
 
           {/* Illustration  */}
           <div className="relative mt-[10%]">
-            <img src="/Sign-up-amico.png" alt="Preview" className="" />
+            <img
+              src="/Sign-up-amico.png"
+              alt="Preview"
+              className="w-[150px] md:w-full"
+            />
           </div>
         </div>
 
         {/* Right Section - Form */}
-        <div className="md:w-1/2 p-8 md:p-12">
-           {/* Form Header */}
-          <h2 className="text-2xl md:text-3xl font-semibold text-gray-700 mb-2">
+        <div className="md:w-1/2 pt-4 pb-4 px-6 md:p-12">
+          {/* Form Header */}
+          <h2 className="text-xl md:text-3xl font-semibold text-gray-700 mb-1 md:mb-2">
             Create Your Account
           </h2>
-          <p className="text-gray-500 mb-6">
-            Join PicNote to save, organize, and remember your notes and images securely.
+          <p className="text-gray-500 text-sm sm:text-md lg:text-[15px] mb-6">
+            Join PicNote to save, organize, and remember your notes and images
+            securely.
           </p>
 
-          <form onSubmit={userRegister} className="space-y-5">
+          <form onSubmit={userRegister} className="space-y-2 md:space-y-5">
             {/* Avatar Upload */}
             <div className="flex gap-7">
               <div className="avatar-preview-container border-2 border-pink-500 xl:h-[20vh] xl:w-[42%] rounded-md flex justify-center items-center overflow-hidden">
@@ -175,14 +180,14 @@ function Register() {
             <button
               type="submit"
               disabled={loading}
-              className="w-full bg-pink-500 text-white py-2 rounded-md font-semibold hover:bg-pink-600 transition cursor-pointer"
+              className="w-full bg-pink-500 mt-3 md:mt-0 text-white py-2 rounded-md font-semibold hover:bg-pink-600 transition"
             >
               {loading ? "Processing..." : "Sign Up"}
             </button>
           </form>
 
           {/* Login Redirect */}
-          <p className="text-center text-gray-600 mt-6">
+          <p className="text-center text-gray-600 mt-3 md:mt-6">
             Already have an account?
             <Link
               to="/login"
