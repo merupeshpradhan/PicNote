f1219129ed54ba434917022631fdada568353ee9
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-10-16 15:08:09 +0530

    Refactor Footer and Navbar components for improved styling consistency and responsiveness

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -4,8 +4,8 @@ import { NavLink } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="bottom-0 left-0 w-full h-[px] bg-gray-900 text-gray-400 py-[25px] flex items-center justify-between px-10">
-      <p>&copy; {new Date().getFullYear()} PicNote. All rights reserved.</p>
+    <footer className="bottom-0 left-0 w-full bg-gray-900 text-gray-400 py-[12px] md:py-[25px] flex items-center justify-between px-3 md:px-10">
+      <p className="text-[13px]">&copy; {new Date().getFullYear()} PicNote. All rights reserved.</p>
       <div className="flex items-center gap-5">
         <NavLink>
           <PiTelegramLogoLight size={19} />
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -52,26 +52,26 @@ function Navbar() {
     }
   };
   return (
-    <nav className="fixed top-0 left-0 w-full bg-white shadow-md py-3 px-10 flex justify-between items-center z-50">
+    <nav className="fixed top-0 left-0 w-full bg-white shadow-md py-2 md:py-3 px-5 md:px-10 flex justify-between items-center z-50">
       <Link to="/">
-        <h1 className="text-3xl font-extrabold text-pink-600 tracking-wide">
+        <h1 className="text-2xl md:text-3xl font-extrabold text-pink-600 tracking-wide">
           Pic<span className="text-gray-800">Note</span>
         </h1>
       </Link>
       <div className="">
         <ul className="flex gap-5">
           {!user ? (
-            <div className="login-register flex gap-5 justify-center items-center">
-              <li className=" flex items-center bg-green-700 py-1.5 px-4 rounded-md text-md font-semibold text-white">
+            <div className="login-register flex gap-3 md:gap-5 justify-center items-center">
+              <li className="flex items-center bg-green-700 py-0.5 px-1.5 md:py-1.5 md:px-4 rounded-md text-md md:font-semibold tracking-wider text-white">
                 <Link to={"/login"}>Login</Link>
               </li>
-              <li className="bg-red-700 py-1.5 px-4 rounded-md text-md font-semibold text-white">
+              <li className="bg-red-700 py-0.5 px-1.5 md:py-1.5 md:px-4 rounded-md text-md md:font-semibold tracking-wider text-white">
                 <Link to={"/register"}>Register</Link>
               </li>
             </div>
           ) : (
             <div className="uerImg-and-name-and-logout flex gap-5 justify-center items-center">
-              <div className="flex justify-center items-center gap-5">
+              <div className="flex justify-center items-center gap-3 md:gap-5">
                 <Link
                   to={"/userDetials"}
                   className="flex justify-center items-center gap-2"
@@ -80,14 +80,14 @@ function Navbar() {
                     <img
                       src={user.avatar}
                       alt={user.userName}
-                      className="w-10 h-10 rounded-full"
+                      className="w-7 h-7 md:w-10 md:h-10 rounded-full"
                     />
                   )}
-                  <span className="font-semibold">{user.userName}</span>
+                  <span className="font-semibold hidden md:block">{user.userName}</span>
                 </Link>
                 <button
                   onClick={handleLogout}
-                  className="border-2 border-red-800 px-2 py-1 rounded-md hover:bg-red-800 hover:text-white font-semibold transition duration-200 cursor-pointer tracking-wide"
+                  className="border md:border-2 border-red-800 px-1.5 py-0.5 md:px-2 md:py-1 rounded-md hover:bg-red-800 hover:text-white text-[12px] md:font-semibold transition duration-200 cursor-pointer font-bold tracking-wider"
                 >
                   Logout
                 </button>
