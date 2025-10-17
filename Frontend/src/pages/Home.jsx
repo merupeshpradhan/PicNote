f432bd28c077af72459a945255661a574db14cd3
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-10-17 20:07:04 +0530

    Refactor Home and CreatePost components for improved layout, styling consistency, and user experience

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,21 +1,40 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Post from "./Posts/Post";
 import { FaPlus } from "react-icons/fa";
 import Footer from "../components/Footer";
+import { toast } from "react-toastify";
 
 function Home() {
   const hideLayout = ["/login", "/register"].includes(location.pathname);
+  const navigate = useNavigate();
+
+  const handleCreatePostClick = () => {
+    const user = localStorage.getItem("user");
+    if (user) {
+      const parsedUser = JSON.parse(user);
+      navigate(`/createPost/${parsedUser._id}`);
+    } else {
+      toast.error("Please Login first", {
+        style: {
+          width: window.innerWidth < 600 ? "250px" : "320px",
+          marginTop: window.innerWidth < 600 ? "10px" : "0px",
+          fontSize: window.innerWidth < 600 ? "18px" : "16px",
+        },
+      });
+      navigate("/login");
+    }
+  };
 
   return (
     <div className="bg-indigo-50 flex flex-col justify-between min-h-screen relative items-center pt-17">
       <Post />
 
-      <Link
-        to={"/createPost/:userId"}
+      <button
+        onClick={handleCreatePostClick}
         className="fixed bottom-4 flex justify-center bg-green-600 text-white p-3.5 rounded-full shadow-lg z-10 hover:bg-green-700 transition duration-300 cursor-pointer"
       >
         <FaPlus size={20} />
-      </Link>
+      </button>
       {!hideLayout && <Footer />}
     </div>
   );
--- Frontend/src/pages/Posts/CreatePost.jsx
+++ Frontend/src/pages/Posts/CreatePost.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Footer from "../../components/Footer";
+import { FaRegImage } from "react-icons/fa";
 
 function CreatePost() {
   const [ImagePreview, setImagePreview] = useState("");
@@ -54,24 +55,26 @@ function CreatePost() {
     <div className="bg-indigo-50 h-[100vh] flex flex-col justify-between items-center gap-12 pt-25">
       <form onSubmit={handlePostCreation} className="flex flex-col gap-5">
         <div className="Image-input-and-view flex gap-5 items-center ">
-          <div className="image-view xl:h-[35vh] xl:w-[15vw] rounded-md border flex justify-center items-center overflow-hidden">
+          <div className="image-view w-[40vw] h-[23vh] md:w-[15vw] md:h-[35vh] rounded-md border flex justify-center items-center overflow-hidden">
             {ImagePreview ? (
-              <div>
+              <div className="post-image-preview">
                 <img
                   src={ImagePreview}
                   alt="Image preview"
-                  className="w-full h-[350px] object-cover"
+                  className="w-full h-full object-cover"
                 />
               </div>
             ) : (
-              <div className="font-semibold text-lime-800">No image</div>
+              <div className="font-semibold text-neutral-500 text-4xl">
+                <FaRegImage />
+              </div>
             )}
           </div>
           <div className="image-input">
             <button
               type="button"
               onClick={() => document.getElementById("fileInput").click()}
-              className="bg-yellow-500 rounded-full p-2.5 transition duration-200 w-[8vw] font-semibold text-white hover:bg-yellow-900 cursor-pointer"
+              className="bg-yellow-500 rounded-full py-2 md:p-2.5 transition duration-200 text-[15px] w-[28vw] md:w-[8vw] font-medium md:font-semibold text-white hover:bg-yellow-900 cursor-pointer"
             >
               {image ? "Change image" : "Chose image"}
             </button>
@@ -94,25 +97,25 @@ function CreatePost() {
         </div>
         <div className="Image-name-with-description">
           <div className="flex flex-col gap-3">
-          {/* Image Name */}
+            {/* Image Name */}
             <input
               type="text"
               placeholder="Image name"
               value={imageName}
               onChange={(e) => setImageName(e.target.value)}
-              className="border w-[24vw] xl:py-2 px-5 font-semibold rounded-md outline-0"
+              className="border md:w-[24vw] py-2 px-2 md:px-5 font-semibold rounded-md outline-0"
             />
             {/* Description */}
             <textarea
               placeholder="Write description..."
               value={description}
               onChange={(e) => setDescription(e.target.value)}
-              className="border  p-2 rounded-md outline-none h-24 resize-none"
+              className="border py-2 px-2 md:p-2 rounded-md outline-none h-24 resize-none"
             ></textarea>
           </div>
         </div>
         <div className="flex justify-center">
-          <button className="bg-green-400 xl:px-6 xl:py-1.5 hover:bg-green-800 rounded transition duration-200 font-semibold tracking-wider text-lg text-white cursor-pointer">
+          <button className="bg-green-500 py-2 xl:px-6 xl:py-1.5 hover:bg-green-700 w-[50%] rounded transition duration-200 font-semibold tracking-widest text-xl md:text-lg text-white cursor-pointer">
             Create
           </button>
         </div>
