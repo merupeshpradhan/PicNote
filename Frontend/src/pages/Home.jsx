b5edad9d5b257104a6c2edd11e75d75c333d7082
Rupesh pradhan <merupeshpradhan@gmail.com>
2026-01-09 17:07:43 +0530

    Refactor button layout: wrap the create post button in a group div for improved hover text visibility and styling consistency.

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -31,12 +31,19 @@ function Home({ postData }) {
     <div className="min-h-[111vh] bg-[#edf5f0] flex flex-col justify-between  relative items-center pt-16 md:pt-16 lg:pt-14 2xl:pt-16">
       <Post postData={postData} />
 
-      <button
-        onClick={handleCreatePostClick}
-        className="fixed bottom-4 flex justify-center bg-green-600 text-white p-3.5 rounded-full shadow-lg z-10 hover:bg-green-700 transition duration-300 cursor-pointer"
-      >
-        <FaPlus size={20} />
-      </button>
+      <div className="group fixed bottom-4 z-10">
+        <button
+          onClick={handleCreatePostClick}
+          className="flex justify-center bg-green-600 text-white p-3.5 rounded-full shadow-lg hover:bg-green-700 transition duration-300 cursor-pointer"
+        >
+          <FaPlus size={20} />
+        </button>
+
+        {/* Hover Text */}
+        <span className="absolute left-1/2 -translate-x-1/2 bottom-14 opacity-0 group-hover:opacity-100 transition bg-black text-white text-xs px-3 py-1 rounded-md whitespace-nowrap">
+          Upload Image
+        </span>
+      </div>
 
       <Footer />
     </div>
